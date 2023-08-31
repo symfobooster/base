eb3d626adab5adb3cba6907b48f3857df20cfd19
zabachok <zabachok@gmail.com>
2023-08-31 14:20:15 +0300

    Fix interface

--- src/Service/TransactionalDecorator.php
+++ src/Service/TransactionalDecorator.php
@@ -5,7 +5,7 @@ namespace Symfobooster\Base\Service;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfobooster\Base\Input\InputInterface;
 
-class TransactionalDecorator
+class TransactionalDecorator implements ServiceInterface
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
